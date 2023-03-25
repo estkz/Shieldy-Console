651fa7adf1f0a15e9fc9734ec935f26a8b5b7afe
estkz <3stetik@gmail.com>
2023-03-25 22:50:18 +0100

    init

---
+++ Shieldy-Console/Shieldy-Console/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Shieldy_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Temporary title
+            Console.WriteLine(" ");
+            Console.WriteLine(" $$$$$$\\  $$\\       $$\\           $$\\       $$\\                $$$$$$\\                      ");
+            Console.WriteLine("$$  __$$\\ $$ |      \\__|          $$ |      $$ |              $$  __$$\\                     ");
+            Console.WriteLine("$$ /  \\__|$$$$$$$\\  $$\\  $$$$$$\\  $$ | $$$$$$$ |$$\\   $$\\     $$ /  $$ | $$$$$$\\   $$$$$$\\  ");
+            Console.WriteLine("\\$$$$$$\\  $$  __$$\\ $$ |$$  __$$\\ $$ |$$  __$$ |$$ |  $$ |    $$$$$$$$ |$$  __$$\\ $$  __$$\\ ");
+            Console.WriteLine("\\____$$\\ $$ |  $$ |$$ |$$$$$$$$ |$$ |$$ /  $$ |$$ |  $$ |    $$  __$$ |$$ /  $$ |$$ /  $$ |");
+            Console.WriteLine("$$\\   $$ |$$ |  $$ |$$ |$$   ____|$$ |$$ |  $$ |$$ |  $$ |    $$ |  $$ |$$ |  $$ |$$ |  $$ |");
+            Console.WriteLine("\\$$$$$$  |$$ |  $$ |$$ |\\$$$$$$$\\ $$ |\\$$$$$$$ |\\$$$$$$$ |$$\\ $$ |  $$ |$$$$$$$  |$$$$$$$  |");
+            Console.WriteLine(" \\______/ \\__|  \\__|\\__| \\_______|\\__| \\_______| \\____$$ |\\__|\\__|  \\__|$$  ____/ $$  ____/ ");
+            Console.WriteLine("                                                $$\\   $$ |              $$ |      $$ |      ");
+            Console.WriteLine("                                                \\$$$$$$  |              $$ |      $$ |      ");
+            Console.WriteLine("                                                 \\______/               \\__|      \\__|      ");
+
+            Console.ReadLine();
+            // 
+        }
+    }
+}
